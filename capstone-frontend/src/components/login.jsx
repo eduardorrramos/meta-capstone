b66bc4ac3ac20b64111529141c466db5596f745b
Eduardo Ramos <eduardoramos@meta.com>
2024-07-02 09:43:01 -0700

    upon log in, user entered into my database with google information

--- capstone-frontend/src/components/login.jsx
+++ capstone-frontend/src/components/login.jsx
@@ -1,11 +1,31 @@
-import {GoogleLogin} from 'react-google-login';
+import { GoogleLogin } from 'react-google-login';
 const clientId = "1095047001559-uqfv7sj3nbph166el14200q2u6rhm38i.apps.googleusercontent.com"
 
 function Login() {
 
     const onSuccess = (res) => {
         console.log("Login Success. User: ", res.profileObj);
+
+        const individual = res.profileObj;
+        const userData = {
+            "email": individual.email,
+            "googleId": individual.googleId,
+            "name": individual.name,
+            "imgUrl": individual.imageUrl
+        };
+        fetch('http://localhost:5000/users', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "http://localhost:5000/users"
+          },
+          body: JSON.stringify(userData)
+        })
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch(error => console.error(error));
     }
+
     const onFailure = (res) => {
         console.log("Login Failure. User: ", res);
     }

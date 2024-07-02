7ece27f3debfc49a0dda1dc02461b6fb5ffb8872
Eduardo Ramos <eduardoramos@meta.com>
2024-07-02 09:43:37 -0700

    keeping in check with frontend commit

--- capstone-backend/index.js
+++ capstone-backend/index.js
@@ -1,10 +1,13 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 const express = require('express')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
-
-
 app.use(express.json());
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
   })
@@ -12,3 +15,24 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send('<h1>Migra</h1>')
   })
+app.use((req, res, next) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Allow requests from http://localhost:5173
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow GET, POST, PUT, and DELETE requests
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Origin'); // Allow Content-Type and Authorization headers
+    res.setHeader('Access-Control-Max-Age', '3600'); // Cache CORS headers for 1 hour
+    next(); // Pass the request to the next middleware or route handler
+  });
+
+app.post('/users', async (req, res) => {
+  const {email, name, googleId, imgUrl} = req.body;
+  const newuser = await prisma.user.create({
+    data: {
+      email, 
+      name,
+      googleId,
+      imgUrl
+    }
+  })
+  res.status(201).json(newuser);
+});
